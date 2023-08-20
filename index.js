9140eab2e30e703474e664ea675edf0709ea59d5
Christofer Lund <christofer.lund@gmail.com>
2023-08-20 17:37:31 +0200

    add files

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-
-const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, GatewayIntentBits, Events, Collection } = require('discord.js');
 const env = require('dotenv').config().parsed
 
+const BOT_TOKEN = env.BOT_TOKEN
+
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-    ]
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages]
 })
 
-client.once('ready', () => {
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`The command at ${filePath} does not have the required data and execute properties.`)
+    }
+}
+
+client.once(Events.ClientReady, () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('Pong!');
-    }
-});
+client.on(Events.InteractionCreate, async interaction => {
+    console.log(interaction)
+})
 
-// Replace 'YOUR_BOT_TOKEN' with your actual bot token
-client.login(env.BOT_TOKEN);
+client.on(Events.MessageCreate, async message => {
+    console.log(message)
+})
+
+client.login(BOT_TOKEN).then(() => {
+    console.log('Logged in!')
+})
