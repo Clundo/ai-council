a04e5070f622a84eea2a435dd6944f36dea8e3af
Christofer Lund <christofer.lund@gmail.com>
2023-08-21 19:04:42 +0200

    add ai

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ client.on(Events.MessageCreate, async message => {
     webhooks.forEach(async webhook => {
         if (webhook.name === message.author.username) return
         const prompt = `
-        In this conversation you should act as ${webhook.name}. This should be a natural conversation, so please reply as briefly and concisely as possible, maximum 200 characters. Respond with only the content part, not the author, and do not repeat previous replies unless specifically asked. Here is the last part of the conversation. : ${JSON.stringify(context)}`
+        In this conversation you should act as ${webhook.name}. This is a natural conversation, so please reply as briefly and concisely as possible. Respond with a string containing only your reply. Here is the last part of the conversation. Only the last message is from the user. The rest is for context. : ${JSON.stringify(context)}`
 
         const response = await openai.chat.completions.create({
             messages: [{role: 'user', content: prompt}], model: 'gpt-3.5-turbo'
